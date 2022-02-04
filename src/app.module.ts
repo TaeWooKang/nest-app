769e10b6d44f983324867444e5a06b772bb04e5c
TaeWooKang <rkdtjsdn123@naver.com>
2022-02-04 17:47:21 +0900

    chore: 사용하지 않는 부분 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
-import { HttpExceptionFilter } from './common/filter/http-exception.filter';
-import { APP_FILTER } from '@nestjs/core';
-
 @Module({
   imports: [CatsModule],
-  providers: [
-    // {
-    //   provide: APP_FILTER,
-    //   useClass: HttpExceptionFilter,
-    // },
-  ],
+  providers: [],
 })
 export class AppModule {}
-// export class AppModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(LoggerMiddleware).forRoutes(CatsController);
-//   }
-// }
